6dc55ab025deacd0d317566338e84a72bfa7eb5b
Kritika <kritika-mac@Kritikas-MacBook-Pro.local>
2023-12-27 14:34:10 +0530

    add product routes and controllers

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 require('dotenv').config({ path: './.env' }); // INFLATE THE ENVIRONMENT VARIABLES
 const dbConnect = require('./db/dbConnect'); // IMPORT THE DB CONNECT FUNCTION
+const productRouter = require('./routes/productRoutes');
+
+// APPLY THE ROUTER MIDDLEWARE WITH A MOUNT PATH
+/**
+ * Whenever a request comes to this route, the handler attached to the
+ * route inside productRoutes will be executed.
+ */
+app.use('/api/v1/products', productRouter);
 
 // CONNECT TO THE DB & START THE SERVER
 dbConnect().then(() => {
